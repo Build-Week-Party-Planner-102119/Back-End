bbb75abf73d29dd0b111fa69defaa170670c3e2e
raberin <rmvaberin@gmail.com>
2019-10-22 16:14:51 -0700

    Fixed login route

--- auth/auth_Router.js
+++ auth/auth_Router.js
@@ -1,30 +1,28 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const Users = require('../users/users_model');
- const secrets = require('../config/secrets')
+const Users = require("../users/users_model");
+const secrets = require("../config/secrets");
 
 // for endpoints beginning with /api/auth
 //GET's all users
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Users.find()
-  .then(users => {
-    res.json(users);
-  })
-  .catch(error => {
-    // console.log(error.message)
-    res.status(200).json({ message: 'cannot add the user', error });
-  });
-})
+    .then(users => {
+      res.json(users);
+    })
+    .catch(error => {
+      // console.log(error.message)
+      res.status(200).json({ message: "cannot add the user", error });
+    });
+});
 
 //Registers new users
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   let user = req.body;
   // console.log(req.body)
-  const hash = bcrypt.hashSync(user.password, 10); 
+  const hash = bcrypt.hashSync(user.password, 10);
   user.password = hash;
 
   Users.add(user)
@@ -33,44 +31,45 @@ router.post('/register', (req, res) => {
     })
     .catch(error => {
       // console.log(error.message)
-      res.status(500).json({ message: 'cannot add the user', error });
+      res.status(500).json({ message: "cannot add the user", error });
     });
 });
 
 //User login
-router.post('/login', (req, res) => {
-  let { username, password } = req.headers;
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+  console.log(req.body);
 
   Users.findBy({ username })
     .first()
     .then(user => {
+      console.log(user);
       if (user && bcrypt.compareSync(password, user.password)) {
         const token = generateToken(user);
+
         res.status(200).json({
           message: `Welcome ${user.username}!`,
-          token,
+          token
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: "Invalid Credentials" });
       }
     })
     .catch(error => {
-      res.status(500).json({ message: 'cannot add the user', error });
+      res.status(500).json({ message: "cannot add the user", error });
     });
 });
 
 function generateToken(user) {
   const payload = {
     subject: user.id,
-    username: user.username,
-    
+    username: user.username
   };
 
   const options = {
-    expiresIn: '12h',
-  }
-  return jwt.sign(payload, secrets.jwtSecret, options)
+    expiresIn: "12h"
+  };
+  return jwt.sign(payload, secrets.jwtSecret, options);
 }
 
 module.exports = router;
-
